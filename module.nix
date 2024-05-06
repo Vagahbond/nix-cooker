50a91b0ae995d8cd9c352ed9ac206dfc7340c442
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-06 14:30:40 +1000

    packages: auto-install

--- module.nix
+++ module.nix
@@ -1,10 +1,49 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (config) theme;
+in {
   imports = [
     ./options.nix
   ];
-  config = {
+
+  config = lib.mkIf (theme != null) {
     fonts.packages = [
-      config.theme.font.package
+      (lib.mkIf
+        (theme.font.package != null)
+        theme.font.package)
+
+      (lib.mkIf
+        (theme.symbolsFont.package != null)
+        theme.symbolsFont.package)
+    ];
+
+    environment.systemPackages = [
+      (lib.mkIf
+        (theme.cursor.package != null)
+        theme.cursor.package)
+
+      (lib.mkIf
+        (theme.gtkTheme.package != null)
+        theme.gtkTheme.package)
+
+      (lib.mkIf
+        (theme.iconsTheme.package != null)
+        theme.iconsTheme.package)
+
+      (lib.mkIf
+        (theme.displayManagerTheme.package != null)
+        theme.displayManagerTheme.package)
+
+      (lib.mkIf
+        (theme.qtTheme.package != null)
+        theme.qtTheme.package)
+
+      (lib.mkIf
+        (theme.wallpaper.package != null)
+        theme.wallpaper.package)
     ];
   };
 }
