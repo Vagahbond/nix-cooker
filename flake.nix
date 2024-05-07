a6ea9ad9a9b6e586dba45d228ad1c82c4e5380c2
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-08 08:18:02 +1000

    include nix-unit

--- flake.nix
+++ flake.nix
@@ -3,11 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nix-unit.url = "github:nix-community/nix-unit";
   };
 
-  outputs = _: {
+  outputs = {
+    nix-unit,
+    nixpkgs,
+    ...
+  }: let
+    allSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = nixpkgs {inherit system;};
+        });
+  in {
     lib = import ./lib;
 
     nixosModules.default = import ./module.nix;
+
+    devShells = forAllSystems ({pkgs}: {
+      default = {
+        nativeBuildInputs = [
+          nix-unit.packages.default
+        ];
+      };
+    });
   };
 }
